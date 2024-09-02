f3e633ebefe455e5061622cc81056dbe1ec98d7f
Jabby Alicante <alicantejabbylumenso@gmail.com>
2024-09-02 17:10:25 +0800

    e-commerce

---
+++ khloris_ecommerce/login.php
@@ -0,0 +1,93 @@
+<?php 
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <input type="checkbox" name="" id="toggler" />
+        <label for="toggler" class="fas fa-bars"></label>
+        <a href="guest.html" class="logo">Khloris<span>.</span></a>
+        <nav class="navbar">
+            <a href="guest.html">Home</a>
+            <a href="#Customization">Customization</a>
+            <a href="flowerpage.html">Flowers</a>
+            <a href="#Occassions">Occassions</a>
+        </nav>
+        <div class="icons">
+            <a href="login.php" class="fas fa-user"></a>
+            <a href="login.php" class="fas fa-shopping-cart"></a>
+
+        </div>
+    </header>
+    <div class="container">
+        <div class="logo-section">
+            <img src="https://res.cloudinary.com/dogrgo15f/image/upload/v1724128500/images/kk90fhhfsltwy7eqgtfg.png"
+                alt="Shop Logo" class="shop-logo">
+
+        </div>
+        <h2 class="shop-name"><span>Khloris</span><br>Flower Shop</h2>
+        <div class="box form-box">
+            <?php 
+             
+              include("php/config.php");
+              if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con,$_POST['email']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                $result = mysqli_query($con,"SELECT * FROM users WHERE email='$email' AND Password='$password'") or die("Select Error");
+                $row = mysqli_fetch_assoc($result);
+
+                if(is_array($row) && !empty($row)){
+                    $_SESSION['valid'] = $row['email'];
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['ID'] = $row['ID'];
+                    
+                    header("Location: mainpage.html");
+                    exit();
+                }else{
+                    echo "<div class='message'>
+                      <p>Wrong Username or Password</p>
+                       </div> <br>";
+                   echo "<a href='login.php'><button class='btn'>Back</button>";
+                }
+              }else{
+            ?>
+
+            <h1>Login</h1>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Email.." autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Password.." autocomplete="off"
+                        required>
+                </div>
+
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Login">
+                </div>
+                <div class="links">
+                    Don't have an account? <a href="register.php">Sign Up here</a>
+                </div>
+            </form>
+        </div>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
---
+++ khloris_ecommerce/register.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+
+    <title>Register</title>
+</head>
+
+<body>
+    <header>
+        <input type="checkbox" name="" id="toggler" />
+        <label for="toggler" class="fas fa-bars"></label>
+        <a href="guest.html" class="logo">Khloris<span>.</span></a>
+        <nav class="navbar">
+            <a href="guest.html">Home</a>
+            <a href="#Customization">Customization</a>
+            <a href="flowerpage.html">Flowers</a>
+            <a href="#Occassions">Occassions</a>
+        </nav>
+        <div class="icons">
+            <a href="login.php" class="fas fa-user"></a>
+            <a href="login.php" class="fas fa-shopping-cart"></a>
+
+        </div>
+
+    </header>
+
+    <div class="container">
+        <div class="logo-section">
+            <img src="https://res.cloudinary.com/dogrgo15f/image/upload/v1724128500/images/kk90fhhfsltwy7eqgtfg.png"
+                alt="Shop Logo" class="shop-logo">
+
+        </div>
+        <h2 class="shop-name"><span>Khloris</span><br>Flower Shop</h2>
+        <div class="box form-box">
+
+            <?php 
+         
+         include("php/config.php");
+         if(isset($_POST['submit'])){
+            $name = $_POST['name'];
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+
+         $verify_query = mysqli_query($con,"SELECT email FROM users WHERE email='$email'");
+
+         if(mysqli_num_rows($verify_query) !=0 ){
+            echo "<div class='message'>
+                      <p>This email is already taken</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO users(name,username,email,Password) VALUES('$name','$username','$email','$password')") or die("Erroe Occured");
+
+            echo "<div class='message'>
+                      <p>Created account successfully</p>
+                  </div> <br>";
+            echo "<a href='login.php'><button class='btn'>Login Now</button>";
+         
+
+         }
+
+         }else{
+         
+        ?>
+
+            <h1>Sign Up</h1>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Name.." autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Username.." autocomplete="off"
+                        required>
+                </div>
+
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Email.." autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Password.." autocomplete="off"
+                        required>
+                </div>
+
+                <div class="field">
+
+                    <input type="submit" class="btn" name="submit" value="Register" required>
+                </div>
+                <div class="links">
+                    Already have an account? <a href="login.php">Sign In</a>
+                </div>
+            </form>
+        </div>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
